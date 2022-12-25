d0bbf3b688b6a42708840c0680e529187cd8c5a4
eliyosef578 <96520067+eliyosef578@users.noreply.github.com>
2022-12-25 11:40:16 +0200

    Add files via upload

--- python-app/send_logs.py
+++ python-app/send_logs.py
@@ -2,6 +2,8 @@ import logging
 import logstash
 import sys
 import random
+import uuid
+from time import sleep
 
 host = 'logstash'
 
@@ -11,47 +13,51 @@ test_logger.addHandler(logstash.LogstashHandler(host, 12000, version=1))
 # test_logger.addHandler(logstash.TCPLogstashHandler(host, 5959, version=1))
 
 
-#test_logger.error('python-logstash: test logstash error message.')
-#test_logger.info('python-logstash: test logstash info message.')
-#test_logger.warning('python-logstash: test logstash warning message.')
-
 # add extra field to logstash message
 
-number_list = ["111", "222", "333", "444", "555"]
-name_list = ["eli", "moshe", "david", "dave", "may"]
-noise_list = ["49.8", "38.5", "98.7", "45.7"]
-#print('python-logstash: test extra fields', extra=extra)
+device_list = ["10010", "23412", "15674", "12202", "12345"]
+version_list = ["0.1.2", "0.7.8", "1.0.0", "1.2.0", "2.0.1"]
+mission_list = ["ex-112", "ol-120", "ta-086", "ma-108"]
+name_list = ["lionel", "eric", "david", "dave", "Emma", "Mia", "Evelyn"]
+noise_list = ["49.8", "38.5", "98.7", "45.7", "48.8", "13.7"]
+componnent_list = ["parser", "sender", "db", "viewer", "handler", "crisper"]
+
+
+
 b=0
 a=0
 count = 0
-from time import sleep
+
 while True:
     sleep(5)
     a = a + 2
     b = b + 3
-    strt = bool(random.getrandbits(1))
-    DeviceID = random.choice(number_list)
-    name1 = random.choice(name_list)
-    noise = random.choice(noise_list)
     count = count + 1
+    status = bool(random.getrandbits(1))
+    DeviceID = random.choice(device_list)
+    Version = random.choice(version_list)
+    name = random.choice(name_list)
+    noise_level = random.choice(noise_list)
+    service = random.choice(componnent_list)
     extra = {
-    'test_string': 'python version: ' + repr(sys.version_info),
-    'test_boolean': strt,
+    # 'test_string': 'python version: ' + repr(sys.version_info),
+    'status': status,
     #'test_dict': {'a': 1, 'b': 'c'},
-    'test_float': a,
-    'test_integer': b,
+    # 'test_float': a,
+    # 'test_integer': b,
     'device_id': DeviceID,
-    'first_name': name1,
-    'noise': noise,
-    #'test_list': [1, 2, '3'],
+    'version': Version,
+    'first_name': name,
+    'noise': noise_level,
+    'service': service,
+    'mission': mission_list, 
+    'cid': str(uuid.uuid4()),
+
 }
     t = {"a":a, "b":b, "count":count}
 
     if count % 2 == 0:
-        test_logger.info('python-logstash: test extra fields', extra=extra)
-        #logger.error('error count: %s, number of message: %s, test: %s',count,a,b)
+        test_logger.info('get message: '+ str(count), extra=extra)
     else:
-        #logger.info('python-logstash: test logstash info message:{} '.format(count))
-        #logger.info('error count: %s, number of message: %s, test: %s',count,a,b)
         print(t)
-        test_logger.error('python-logstash: error', extra=extra)
+        test_logger.error('error: ' + str(count), extra=extra)
